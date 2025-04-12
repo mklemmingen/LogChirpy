a6240ad98d34425e3fcdac77ac419227d818d502
Luis Wehrberger <luis.wehrberger2@gmail.com>
2025-04-12 13:12:28 +0200

    add forgot password page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen name="signup" options={{ headerShown: false }} />
+          <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       </View>
