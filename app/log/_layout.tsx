ba3c9c2f0f07dada40197891383fe56a6f1f867d
Martin (Marty) Lauterbach <104225647+mklemmingen@users.noreply.github.com>
2025-04-28 08:25:13 +0200

    changed config

--- app/log/_layout.tsx
+++ app/log/_layout.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-import { LogDraftProvider } from '../context/LogDraftContext';
 
 export default function LogLayout({ children }: { children: React.ReactNode }) {
     return (
-        <LogDraftProvider>
-            <Stack screenOptions={{ headerShown: false }}>
-                {children}
-            </Stack>
-        </LogDraftProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+            {children}
+        </Stack>
     );
 }
