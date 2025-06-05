6f2ba0d0036d609a8fadc05c824b6581b5b2fb77
Martin (Marty) Lauterbach <104225647+mklemmingen@users.noreply.github.com>
2025-06-05 20:14:52 +0200

    refactor: update Android build script and improve metro configuration for better compatibility

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
-import { Platform } from 'react-native';
-
-// Must be exported or Fast Refresh won't update the context
-export function App() {
-  const ctx = require.context('./app');
-  return <ExpoRoot context={ctx} />;
-}
-
-registerRootComponent(App);
+import 'expo-router/entry';
