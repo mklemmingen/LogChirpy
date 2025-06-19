691a92a1bcf47277194c1794ddc2629b992064cc
Youmna Samouneh <youmna.samouneh@Student.Reutlingen-University.de>
2025-06-19 09:37:53 +0200

    Fehlerbehebung: module-resolver Plugin für @-Alias hinzugefügt (Metro konnte Alias nicht auflösen)

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,15 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin'
-    ]
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './', 
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
