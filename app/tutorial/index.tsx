52a5886e97dd2f119492128082816c1602949306
Youmna Samouneh <youmna.samouneh@Student.Reutlingen-University.de>
2025-06-23 12:07:46 +0200

    Überarbeitete Übersetzung für das Tutorial

--- app/tutorial/index.tsx
+++ app/tutorial/index.tsx
@@ -188,6 +188,6 @@ export default function TutorialScreen() {
 const styles = StyleSheet.create({
     tutorialCard: { padding: 0 },
     tutorialContent: { padding: 20, gap: 16 },
-    tutorialSectionTitle: { fontWeight: '600', marginTop: 40, marginBottom: 40 },
+    tutorialSectionTitle: { fontWeight: '600', marginBottom: 10 },
     tutorialText: { lineHeight: 20 },
 });
